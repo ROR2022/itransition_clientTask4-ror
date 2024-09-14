61ad2152443b918196574573c05304a091af1530
Ramiro Ocampo Rodriguez <101475348+ROR2022@users.noreply.github.com>
2024-09-14 10:57:56 -0600

    videos

--- src/components/Videos/Videos.tsx
+++ src/components/Videos/Videos.tsx
@@ -5,13 +5,24 @@ const Videos = () => {
     <div
         style={{
             display: 'flex',
+            flexDirection: 'column',
             justifyContent: 'center',
             alignItems: 'center',
             padding: '20px',
         }}
     >
-        <video src="/video1Task4.mp4" width={600} height={800} controls></video>
-        <video src="/video2Task4.mp4" width={600} height={800} controls></video>
+        <h2
+            style={{
+                marginBottom: '20px',
+            }}
+        >Video 1</h2>
+        <video src="/video1Task4.mp4" width={800} height={1000} controls></video>
+        <h2
+            style={{
+                marginBottom: '20px',
+            }}
+        >Video 2</h2>
+        <video src="/video2Task4.mp4" width={800} height={1000} controls></video>
     </div>
   )
 }
