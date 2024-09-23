fa9656c4d14380e5fba0f7b42dab43bfc7f5d0ff
Ramiro Ocampo Rodriguez <101475348+ROR2022@users.noreply.github.com>
2024-09-23 14:06:29 -0600

    presentations

---
+++ src/app/video-task6/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import VideoTask6 from '@/components/Videos/VideoTask6'
+
+const page = () => {
+  return (
+    <div>
+        <VideoTask6 />
+    </div>
+  )
+}
+
+export default page
---
+++ src/components/Videos/VideoTask6.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const VideoTask6 = () => {
+  return (
+    <div
+        style={{
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center',
+            alignItems: 'center',
+            padding: '20px',
+        }}
+    >
+        
+        <video src="/videoTask6.mp4" width={1000} height={800} controls></video>
+    </div>
+  )
+}
+
+export default VideoTask6
