821b541ed65ad2c460abb7271833ec7559da8fc1
Ramiro Ocampo Rodriguez <101475348+ROR2022@users.noreply.github.com>
2024-09-28 17:45:42 -0600

    debug1

--- src/components/Chat/Chat.tsx
+++ src/components/Chat/Chat.tsx
@@ -1,11 +1,17 @@
 "use client";
-import React from "react";
+import React,{ useEffect} from "react";
 import DataMainUser from "./DataMainUser";
 import { ChatProvider } from "./context/ChatContext";
 import ChatLayout from "./ChatLayout";
+import { useContext } from "react";
+import { ChatContext } from "./context/ChatContext";
 
 const Chat = () => {
+  const { dataUserChat } = useContext(ChatContext);
   
+  useEffect(() => {
+    
+  },[dataUserChat._id]);
 
   return (
     <ChatProvider>
@@ -19,7 +25,10 @@ const Chat = () => {
           }}
         >
           <DataMainUser />
+          {dataUserChat._id === "" ? <p>No User</p> : 
           <ChatLayout />
+          }
+          
         </div>
       }
     </ChatProvider>
